c432a894aed1ddec2315558516c167d4fe651a2b
Michael Coon <mac718@gmail.com>
2016-07-27 11:49:10 -0400

    Add exisiting file

---
+++ Euler_pr3.js
@@ -0,0 +1,31 @@
+var largestPrime;
+var number = 13195;
+var list = [];
+
+for (var i = 2; i <= number/2; i++){
+	if(number % i === 0){
+  	list.push(i);
+  }
+}
+
+console.log(list);
+
+
+for (var i = 0; i < list.length; i++){
+console.log(list[i]);
+
+	if (list[i] % 2 === 0 || list[i] % 3 === 0 && list[i] > 3){
+  	list.splice(i,1);
+  } else {
+  	var j = 7;
+  	while(j < list[i]){
+    	if(list[i] % j === 0 || list[i] % (j+2) === 0 && list[i] != (j+2)){
+      	list.splice(i,2);
+       	};
+      j += 2;
+     };
+    };
+   };
+  
+ largestPrime = list.pop();
+ 
