5c186af5aa084c8de1b186db2a3c0bd9ee31b3da
Michael Coon <mac718@gmail.com>
2016-07-28 17:11:03 -0400

    update Euler_pr3.js

--- Euler_pr3.js
+++ Euler_pr3.js
@@ -8,24 +8,19 @@ for (var i = 2; i <= number/2; i++){
   }
 }
 
-console.log(list);
-
-
 for (var i = 0; i < list.length; i++){
-console.log(list[i]);
-
 	if (list[i] % 2 === 0 || list[i] % 3 === 0 && list[i] > 3){
-  	list.splice(i,1);
+  		list.splice(i,1);
   } else {
-  	var j = 7;
-  	while(j < list[i]){
-    	if(list[i] % j === 0 || list[i] % (j+2) === 0 && list[i] != (j+2)){
-      	list.splice(i,2);
-       	};
-      j += 2;
-     };
-    };
-   };
+  		var j = 7;
+  		while(j < list[i]){
+    		if(list[i] % j === 0 || list[i] % (j+2) === 0 && list[i] != (j+2)){
+      		list.splice(i,2);
+       		};
+      	j += 2;
+     	};
+   	};
+ };
   
  largestPrime = list.pop();
  
